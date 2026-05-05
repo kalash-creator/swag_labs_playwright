7132c7f85f5e46168d4b13d6b1194e9a4ddc24c5
kalash-creator <kalashtan87@gmail.com>
2026-05-05 19:20:17 +0300

    structure

--- utils/config.py
+++ utils/config.py
@@ -9,4 +9,10 @@ LOCKED_USER = os.getenv("LOCKED_USER")
 PASSWORD = os.getenv("PASSWORD")
 BROWSER = os.getenv("BROWSER", "chromium")
 HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
+VALID_USERNAME = os.getenv("STANDARD_USER")
+VALID_PASSWORD = os.getenv("PASSWORD")
+LOCKED_OUT_USERNAME = os.getenv("LOCKED_USER")
+LOCKED_OUT_PASSWORD = os.getenv("PASSWORD")
+INVALID_USERNAME = os.getenv("FAULTY_USERNAME")
+INVALID_PASSWORD = os.getenv("FAULTY_PASSWORD")
 
